450cdbbd6359d571681fac0a554403d2f8b739e9
williamneto <william.g.neto@gmail.com>
2013-03-22 14:43:25 -0300

    close 2

--- src/com/naopassedaconta/AddDespesaActivity.java
+++ src/com/naopassedaconta/AddDespesaActivity.java
@@ -6,6 +6,7 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import com.naopassedaconta.core.db.DatabaseHelper;
 import com.naopassedaconta.despesas.models.Despesa;
@@ -24,22 +25,26 @@ public class AddDespesaActivity extends Activity {
 	}
 	
 	private OnClickListener clickAddDespesa = new OnClickListener() {
-		public void onClick(View v) {
-			Despesa d = new Despesa();
-			
+		public void onClick(View v) {			
 			String descricao = ((EditText) findViewById(R.id.descricao_text)).getText().toString();
 			String valor = ((EditText) findViewById(R.id.valor)).getText().toString();
 			
-			d.id = null;
-			d.descricao = descricao;
-			d.valor = valor;
-			
-			d.save();
-			
-			((EditText) findViewById(R.id.descricao_text)).setText("");
-			((EditText) findViewById(R.id.valor)).setText("");
-			
-			finish();
+			if (descricao.length() == 0 || valor.length() == 0){
+				((TextView) findViewById(R.id.text2)).setText("Preencha descrição e valor");
+			}else{
+				Despesa d = new Despesa();
+				d.id = null;
+				d.descricao = descricao;
+				d.valor = valor;
+				
+				d.save();
+				
+				((EditText) findViewById(R.id.descricao_text)).setText("");
+				((EditText) findViewById(R.id.valor)).setText("");
+				((TextView) findViewById(R.id.text2)).setText("");
+				
+				finish();
+			}
 		}
 	};
 
